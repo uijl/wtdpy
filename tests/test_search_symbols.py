192f39496478e547e51d3f6c432afbc83ed216bf
Joris den Uijl <jorisdenuijl@gmail.com>
2020-03-20 07:41:21 +0100

    Test to see whether the CircleCI environment variables can be reached.

--- tests/test_search_symbols.py
+++ tests/test_search_symbols.py
@@ -1,4 +1,11 @@
 from wtdpy import WTDpy
+import httpx
 
-def test_basic_test():
-    assert 3 == 2 + 1
+import os
+
+params = {"api_token": os.environ["api_key"], "symbol": "^AEX"}
+
+def test_basic_test(params):
+    
+    request_url = "https://api.worldtradingdata.com/api/v1/history"
+    httpx.get(request_url, params=params)
